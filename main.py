436a3fd3aacd9a88739b7773b3b406ee13404e46
Giovanni <giovannibarros_99@hotmail.com>
2025-05-13 17:47:16 -0300

    temp

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-
 import pandas as pd
 from preprocessing import discretize, encode_features
-from mining import mine_rules
+from mining import mine_rules, clean_rules
+
 
 def main():
     df = pd.read_csv('alzheimers_disease_data.csv')
-    print(f'df_bruto:\n ${df.head(10)}')
     df = discretize(df)
-    print(f'df_discretizado:\n ${df.head(10)}')
     df_encoded = encode_features(df)
-    print(f'df_transacional:\n ${df_encoded.head(10)}')
     basket = pd.get_dummies(df_encoded)
-    
-    rules_alzheimer, rules_no_alzheimer = mine_rules(basket, 0.2, 0.6)
-    
-    rules_alzheimer.to_csv('alzheimers_association_rules_Diagnosis_1.csv', index=False)
-    rules_no_alzheimer.to_csv('alzheimers_association_rules_Diagnosis_0.csv', index=False)
-    
+
+    rules_alzheimer, rules_no_alzheimer = mine_rules(basket, 0.5, 0.7)
+    rules_alzheimer = clean_rules(rules_alzheimer)
+    rules_no_alzheimer = clean_rules(rules_no_alzheimer)
+
+    rules_alzheimer.to_csv(
+        'alzheimers_association_rules_Diagnosis_1.csv', index=False)
+    rules_no_alzheimer.to_csv(
+        'alzheimers_association_rules_Diagnosis_0.csv', index=False)
+
     print("=== Regras para Diagnosis = 1 (Alzheimer) ===")
-    print(rules_alzheimer[['antecedents', 'consequents', 'support', 'confidence', 'lift']].head(15))
-    
+    print(rules_alzheimer[['antecedents', 'consequents',
+          'support', 'confidence', 'lift']].head(15))
+
     print("\n=== Regras para Diagnosis = 0 (Sem Alzheimer) ===")
-    print(rules_no_alzheimer[['antecedents', 'consequents', 'support', 'confidence', 'lift']].head(15))
+    print(rules_no_alzheimer[['antecedents', 'consequents',
+          'support', 'confidence', 'lift']].head(15))
+
 
 if __name__ == "__main__":
     main()
--- mining.py
+++ mining.py
@@ -1,14 +1,33 @@
-
 from mlxtend.frequent_patterns import apriori, association_rules
 
+
 def mine_rules(basket, min_support=0.3, min_confidence=0.6):
-    frequent_itemsets = apriori(basket, min_support=min_support, use_colnames=True)
-    rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=min_confidence)
-    
-    rules_alzheimer = rules[rules['consequents'].astype(str).str.contains('Diagnosis=1')]
-    rules_no_alzheimer = rules[rules['consequents'].astype(str).str.contains('Diagnosis=0')]
-    
-    rules_alzheimer = rules_alzheimer.sort_values(by='confidence', ascending=False)
-    rules_no_alzheimer = rules_no_alzheimer.sort_values(by='confidence', ascending=False)
-    
+    frequent_itemsets = apriori(
+        basket, min_support=min_support, use_colnames=True)
+    rules = association_rules(
+        frequent_itemsets, metric="confidence", min_threshold=min_confidence)
+
+    rules_alzheimer = rules[rules['consequents'].astype(
+        str).str.contains('Diagnosis=1')]
+    rules_no_alzheimer = rules[rules['consequents'].astype(
+        str).str.contains('Diagnosis=0')]
+
+    rules_alzheimer = rules_alzheimer.sort_values(
+        by='confidence', ascending=False)
+    rules_no_alzheimer = rules_no_alzheimer.sort_values(
+        by='confidence', ascending=False)
+
     return rules_alzheimer, rules_no_alzheimer
+
+
+def clean_rules(rules):
+    def clean_frozenset(frozenset_obj):
+        return ', '.join(sorted(list(frozenset_obj)))
+
+    rules = rules.copy()
+    rules['antecedents'] = rules['antecedents'].apply(clean_frozenset)
+    rules['consequents'] = rules['consequents'].apply(clean_frozenset)
+
+    selected_cols = ['antecedents', 'consequents', 'antecedent support',
+                     'consequent support', 'support', 'confidence', 'lift']
+    return rules[selected_cols]
