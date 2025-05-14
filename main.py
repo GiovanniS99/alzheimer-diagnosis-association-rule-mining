f67c2237e53b4e5f2f799cde7e811516cfb4d4b6
Giovanni <giovannibarros_99@hotmail.com>
2025-05-14 12:00:41 -0300

    adicionar pasta com dados e ignorar pasta de saída

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mining import mine_rules, clean_rules, remove_duplicate_rules, prune_rules
 
 
 def main():
-    df = pd.read_csv('alzheimers_disease_data.csv')
+    df = pd.read_csv('data/alzheimers_disease_data.csv')
     df = discretize(df)
     df_encoded = encode_features(df)
     basket = pd.get_dummies(df_encoded)
@@ -18,9 +18,9 @@ def main():
     rules_no_alzheimer = remove_duplicate_rules(rules_no_alzheimer)
 
     rules_alzheimer.to_csv(
-        'alzheimers_association_rules_Diagnosis_1.csv', index=False)
+        'output/alzheimers_association_rules_Diagnosis_1.csv', index=False)
     rules_no_alzheimer.to_csv(
-        'alzheimers_association_rules_Diagnosis_0.csv', index=False)
+        'output/alzheimers_association_rules_Diagnosis_0.csv', index=False)
 
     print("=== Regras para Diagnosis = 1 (Alzheimer) ===")
     print(rules_alzheimer.head(15))
